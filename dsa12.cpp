1b0302833438cc88523d9745da4f690c6d60bb92
studentOss <yadavshita1511@gmail.com>
2025-05-04 19:50:10 +0530

    Update dsa12.cpp

--- dsa12.cpp
+++ dsa12.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <cstring>
+#include <map> // ADD THIS FOR INDEX
 using namespace std;
 
 class Record {
     int id;
-    string name;
+    char name[50];
     int salary;
-    string designation;
+    char designation[50];
 
 public:
     Record();
@@ -17,99 +19,101 @@ public:
 
 Record::Record() {
     id = 0;
-    name = ' ';
-    designation = ' ';
+    strcpy(name, "");
     salary = 0;
+    strcpy(designation, "");
 }
 
 int Record::getIdno() {
-    return (id);
+    return id;
 }
 
 void Record::getData() {
     cout << "\nEnter Details: ";
     cout << "\nId no: ";
     cin >> id;
+    cin.ignore();
+
     cout << "Name: ";
-    cin >> name;
+    cin.getline(name, 50);
+
     cout << "Salary: ";
     cin >> salary;
+    cin.ignore();
+
     cout << "Designation: ";
-    cin >> designation;
+    cin.getline(designation, 50);
 }
 
 void Record::putData() {
-    cout << "\nId No.: ";
-    cout << id;
-    cout << "\t\tName: ";
-    cout << name;
-    cout << "\nSalary: ";
-    cout << salary;
-    cout << "\tDesignation: ";
-    cout << designation;
+    cout << "\nId No.: " << id;
+    cout << "\t\tName: " << name;
+    cout << "\nSalary: " << salary;
+    cout << "\tDesignation: " << designation;
 }
-
 class File {
     ifstream fin;
     ofstream fout;
     fstream fs;
-
+    map<int, streampos> index; // INDEX MAP
 public:
     void insert();
     void display();
     void search(int);
     int Delete(int);
     int edit(int);
+    void buildIndex(); // BUILD INDEX FUNCTION
 };
-
+void File::buildIndex() {
+    index.clear();
+    Record r;
+    fin.open("EmployeeDB", ios::binary);
+    while (fin.read((char*)&r, sizeof(r))) {
+        streampos pos = fin.tellg();
+        index[r.getIdno()] = pos - sizeof(r);
+    }
+    fin.close();
+}
 void File::insert() {
     Record r;
     r.getData();
-    fout.open("EmployeeDB", ios::ate | ios::app);
+    fout.open("EmployeeDB", ios::binary | ios::app);
     fout.write((char *)&r, sizeof(r));
     fout.close();
 }
-
 void File::display() {
     Record r;
-    fin.open("EmployeeDB");
+    fin.open("EmployeeDB", ios::binary);
     fin.seekg(0, ios::beg);
     while (fin.read((char *)&r, sizeof(r)))
         r.putData();
     fin.close();
 }
-
 void File::search(int id) {
+    buildIndex();
     Record r;
-    int flag = 0;
-    fin.open("EmployeeDB");
-    fin.seekg(0, ios::beg);
-    while (fin.read((char *)&r, sizeof(r))) {
-        if (r.getIdno() == id) {
-            flag = 1;
-            break;
-        }
-    }
-    fin.close();
-    if (flag == 1) {
+    //if (index.find(id) != index.end()) {
+    if(index.contains(id){
+        fin.open("EmployeeDB", ios::binary);
+        fin.seekg(index[id]);
+        fin.read((char*)&r, sizeof(r));
         cout << "\nRecord Found:";
         r.putData();
-    }
-    else
+        fin.close();
+    } else {
         cout << "\nRecord not Found ";
+    }
 }
-
 int File::Delete(int id) {
     Record r;
     int flag = 0;
-    fin.open("EmployeeDB");
-    fout.open("Temp", ios::ate | ios::app);
+    fin.open("EmployeeDB", ios::binary);
+    fout.open("Temp", ios::binary | ios::app);
     fin.seekg(0, ios::beg);
     while (fin.read((char *)&r, sizeof(r))) {
         if (r.getIdno() == id) {
             flag = 1;
-        }
-        else {
+        } else {
             fout.write((char *)&r, sizeof(r));
         }
     }
@@ -117,25 +121,25 @@ int File::Delete(int id) {
     fout.close();
     remove("EmployeeDB");
     rename("Temp", "EmployeeDB");
-    return (flag);
+    return flag;
 }
 
 int File::edit(int id) {
+    buildIndex();
     Record r;
     int flag = 0;
-    fs.open("EmployeeDB");
-    fs.seekg(0, ios::beg);
-    while (fs.read((char *)&r, sizeof(r))) {
-        if (r.getIdno() == id) {
-            flag = 1;
-            cout << "\nEnter New Details: ";
-            r.getData();
-            fs.seekp((int)fs.tellg() - sizeof(r), ios::beg);
-            fs.write((char *)&r, sizeof(r));
-        }
+    if (index.find(id) != index.end()) {
+        fs.open("EmployeeDB", ios::binary | ios::in | ios::out);
+        fs.seekg(index[id]);
+        fs.read((char*)&r, sizeof(r));
+        cout << "\nEnter New Details: ";
+        r.getData();
+        fs.seekp(index[id]);
+        fs.write((char*)&r, sizeof(r));
+        fs.close();
+        flag = 1;
     }
-    fs.close();
-    return (flag);
+    return flag;
 }
 
 int main() {
@@ -152,55 +156,54 @@ int main() {
         cout << "\n7. Quit";
         cout << "\nEnter your Choice: ";
         cin >> ch;
+
         switch (ch) {
-            case 1:
-                if (flag == 0) {
-                    cout << "\nEnter No of Records to insert : ";
-                    cin >> n;
-                    for (i = 0; i < n; i++) {
-                        f.insert();
-                    }
-                    flag = 1;
+        case 1:
+            if (flag == 0) {
+                cout << "\nEnter No of Records to insert : ";
+                cin >> n;
+                for (i = 0; i < n; i++) {
+                    f.insert();
                 }
-                else {
-                    cout << "\nSorry.. Table is Already build... \n If want to add record please select Insert a New Entry in option.....";
-                }
-                break;
-            case 2:
-                f.display();
-                break;
-            case 3:
-                f.insert();
-                break;
-            case 4:
-                cout << "\nEnter Id No of Employee Whose Record is to be Deleted: ";
-                cin >> n;
-                i = f.Delete(n);
-                if (i == 1)
-                    cout << "\nRecord Deleted Successfully";
-                else
-                    cout << "\nRecord not Found";
-                break;
-            case 5:
-                cout << "\nEnter Id No of Employee Whose Record is to be Edit: ";
-                cin >> n;
-                i = f.edit(n);
-                if (i == 1)
-                    cout << "\nRecord Modified Successfully";
-                else
-                    cout << "\nRecord not Found";
-                break;
-            case 6:
-                cout << "\nEnter Id No of Employee Whose Record is to be Searched: ";
-                cin >> n;
-                f.search(n);
-                break;
-            case 7:
-                break;
-            default:
-                cout << "\nEnter Valid Choice.....";
+                flag = 1;
+            } else {
+                cout << "\nSorry.. Table is Already built...\nIf you want to add a record, please select Insert a New Entry from the menu.";
+            }
+            break;
+        case 2:
+            f.display();
+            break;
+        case 3:
+            f.insert();
+            break;
+        case 4:
+            cout << "\nEnter Id No of Employee Whose Record is to be Deleted: ";
+            cin >> n;
+            i = f.Delete(n);
+            if (i == 1)
+                cout << "\nRecord Deleted Successfully";
+            else
+                cout << "\nRecord not Found";
+            break;
+        case 5:
+            cout << "\nEnter Id No of Employee Whose Record is to be Edited: ";
+            cin >> n;
+            i = f.edit(n);
+            if (i == 1)
+                cout << "\nRecord Modified Successfully";
+            else
+                cout << "\nRecord not Found";
+            break;
+        case 6:
+            cout << "\nEnter Id No of Employee Whose Record is to be Searched: ";
+            cin >> n;
+            f.search(n);
+            break;
+        case 7:
+            break;
+        default:
+            cout << "\nEnter Valid Choice.....";
         }
     } while (ch != 7);
-    return (0);
+    return 0;
 }
-
